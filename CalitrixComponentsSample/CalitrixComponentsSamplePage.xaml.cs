50912c3c317f60bc3d58afac6c2ddac0a9dd30de
Allan Melo <allan.melo@liferay.com>
2017-12-01 14:54:53 -0300

    Init commit

---
+++ CalitrixComponentsSample/CalitrixComponentsSamplePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CalitrixComponentsSample
+{
+    public partial class CalitrixComponentsSamplePage : ContentPage
+    {
+        public CalitrixComponentsSamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
